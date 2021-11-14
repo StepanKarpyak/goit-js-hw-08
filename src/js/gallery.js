fb3eb92d7e5e98eaa746578224e5d33e5bee1cb2
StepanKarpyak <stepan_karpyak@ukr.net>
2021-11-14 10:21:12 +0200

    створення

---
+++ src/js/gallery.js
@@ -0,0 +1,47 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Add imports above this line
+import { galleryItems } from './app';
+// Change code below this line
+
+const galleryRef = document.querySelector('.gallery');
+
+// const createMarkup = images =>
+//     images
+//         .map(({ preview, original, description }) => {
+//             return `
+//     <a href="${original}">
+//         <img src="${preview}" alt="${description}" title="${description}" />
+//     </a>`;
+//         })
+//         .join('');
+// const markup = createMarkup(galleryItems);
+const createMarkup = ({ original, preview, description }) => {
+    return `<a href=${original}>
+  <img  src=${preview} alt=${description} />
+</a>
+`;
+};
+
+const markup = galleryItems.map((item) => createMarkup(item)).join('');
+
+const renderMarkup = strings => {
+    galleryRef.insertAdjacentHTML('beforeend', strings);
+};
+
+renderMarkup(markup);
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 500,
+});
+
+const onClick = e => {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+        return;
+    } else {
+        lightbox.open(e.target);
+    }
+};
+galleryRef.addEventListener('click', onClick);
