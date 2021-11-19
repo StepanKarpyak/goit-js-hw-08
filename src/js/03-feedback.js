b40da4e1da116cd0cb143ec83d6178a870982e25
StepanKarpyak <stepan_karpyak@ukr.net>
2021-11-19 21:46:51 +0200

    корекція фітбека

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,3 +1,14 @@
+// Відстежуй на формі подію input, і щоразу записуй у локальне сховище об'єкт з полями email
+// і message, у яких зберігай поточні значення полів форми.Нехай ключем для сховища буде 
+// рядок "feedback-form-state".
+// Під час завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані,
+//  заповнюй ними поля форми.В іншому випадку поля повинні бути порожніми.
+// Під час сабміту форми очищуй сховище і поля форми, а також виводь у консоль об'єкт з 
+// полями email, message та їхніми поточними значеннями.
+// Зроби так, щоб сховище оновлювалось не частіше, ніж раз на 500 мілісекунд.Для цього додай
+//  до проекту і використовуй бібліотеку lodash.throttle.
+
+
 const throttle = require('lodash.throttle');
 
 const formRef = document.querySelector('.feedback-form');
@@ -27,11 +38,17 @@ const storageValuesLogger = () => {
 };
 
 const onFormSubmit = e => {
+
     e.preventDefault();
-    // console.log(localStorage.getItem(FORM_FIELDS_KEY));
-    storageValuesLogger();
-    e.currentTarget.reset();
-    localStorage.removeItem(FORM_FIELDS_KEY);
+    try {
+        console.log(localStorage.getItem(FORM_FIELDS_KEY));
+        storageValuesLogger();
+        e.currentTarget.reset();
+        localStorage.removeItem(FORM_FIELDS_KEY);
+    } catch (error) {
+        return alert('всі поля повинні бути заповнені!')
+    }
+
 };
 
 const populateTextMessage = () => {
